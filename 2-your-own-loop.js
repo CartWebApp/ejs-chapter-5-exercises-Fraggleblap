1428da7f5fd55a0b2436095ccc43fed2667e62bf
Fraggleblap <Owen.Gaffney23@student.cart.org>
2024-02-15 14:29:53 -0800

    works

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,6 +1,11 @@
 // Your code here.  
-function loop(numberOne) {
-    return numberO
+function loop(numberOne, test, updater, print) {
+    let temp = numberOne;
+    while (test(temp)) {
+        print(temp);
+        temp = updater(temp);
+    }
+    return print;
 }
 
 loop(3, n => n > 0, n => n - 1, console.log);
